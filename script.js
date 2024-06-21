5d1dca6fdf85e2ae926f491e9f18905cfbae5d36
skaur2324 <105301202+skaur2324@users.noreply.github.com>
2024-06-21 16:09:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,149 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const checkbox = document.querySelector('label input[type="checkbox"]');
+    const links = document.querySelectorAll('label ul a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function() {
+            checkbox.checked = false;
+        });
+    });
+
+    const header = document.querySelector('header');
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+
+    let w, h, balls = [];
+    let mouse = {
+        x: undefined,
+        y: undefined
+    };
+    let rgb = [
+        "rgb(26, 188, 156)",
+        "rgb(46, 204, 113)",
+        "rgb(52, 152, 219)",
+        "rgb(155, 89, 182)",
+        "rgb(241, 196, 15)",
+        "rgb(230, 126, 34)",
+        "rgb(231, 76, 60)"
+    ];
+
+    function init() {
+        resizeReset();
+        animationLoop();
+    }
+
+    function resizeReset() {
+        w = canvas.width = header.clientWidth;
+        h = canvas.height = header.clientHeight;
+    }
+
+    function animationLoop() {
+        ctx.clearRect(0, 0, w, h);
+        ctx.globalCompositeOperation = 'lighter';
+        drawBalls();
+
+        let temp = [];
+        for (let i = 0; i < balls.length; i++) {
+            if (balls[i].time <= balls[i].ttl) {
+                temp.push(balls[i]);
+            }
+        }
+        balls = temp;
+
+        requestAnimationFrame(animationLoop);
+    }
+
+    function drawBalls() {
+        for (let i = 0; i < balls.length; i++) {
+            balls[i].update();
+            balls[i].draw();
+        }
+    }
+
+    function mousemove(e) {
+        const rect = canvas.getBoundingClientRect();
+        mouse.x = e.clientX - rect.left;
+        mouse.y = e.clientY - rect.top;
+
+        for (let i = 0; i < 3; i++) {
+            balls.push(new Ball());
+        }
+    }
+
+    function mouseout() {
+        mouse.x = undefined;
+        mouse.y = undefined;
+        balls = []; // Clear balls array on mouseout to stop the effect immediately
+    }
+
+    function getRandomInt(min, max) {
+        return Math.round(Math.random() * (max - min)) + min;
+    }
+
+    function easeOutQuart(x) {
+        return 1 - Math.pow(1 - x, 4);
+    }
+
+    class Ball {
+        constructor() {
+            this.start = {
+                x: mouse.x + getRandomInt(-20, 20),
+                y: mouse.y + getRandomInt(-20, 20),
+                size: getRandomInt(30, 40)
+            };
+            this.end = {
+                x: this.start.x + getRandomInt(-300, 300),
+                y: this.start.y + getRandomInt(-300, 300)
+            };
+
+            this.x = this.start.x;
+            this.y = this.start.y;
+            this.size = this.start.size;
+
+            this.style = rgb[getRandomInt(0, rgb.length - 1)];
+
+            this.time = 0;
+            this.ttl = 120;
+        }
+
+        draw() {
+            ctx.fillStyle = this.style;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
+        }
+
+        update() {
+            if (this.time <= this.ttl) {
+                let progress = 1 - (this.ttl - this.time) / this.ttl;
+
+                this.size = this.start.size * (1 - easeOutQuart(progress));
+                this.x = this.x + (this.end.x - this.x) * 0.01;
+                this.y = this.y + (this.end.y - this.y) * 0.01;
+            }
+            this.time++;
+        }
+    }
+
+    canvas.addEventListener("mousemove", mousemove);
+    canvas.addEventListener("mouseout", mouseout);
+    window.addEventListener("resize", resizeReset);
+
+    init();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
